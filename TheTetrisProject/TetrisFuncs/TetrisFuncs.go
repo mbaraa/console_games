a10fd7b97dcf0d27b8d5a0d93f1a5639fa397f62
Baraa Al-Masri <hexagon16rpm@yandex.com>
2020-08-23 05:12:45 +0300

    got something working

--- TheTetrisProject/TetrisFuncs/TetrisFuncs.go
+++ TheTetrisProject/TetrisFuncs/TetrisFuncs.go
@@ -1,21 +1,31 @@
 package TetrisFuncs
 
-import Const "../Constants"
+import (
+	"fmt"
+	"os"
+
+	Const "../Constants"
+	Common "../Shared"
+)
 
 // initializers:
 
 // initialize lengths with row length - 1
 func InitLengths(lengths *[Const.COLUMNS]int) {
+
 	for i := 0; i < Const.COLUMNS; i++ {
 		(*lengths)[i] = Const.ROWS - 1
 	} // for
+
 } // initLengths()
 
 // initialize completed lines with falses(0)
 func InitCompletedLines(completedLines *[Const.ROWS]bool) {
+
 	for i := 0; i < Const.ROWS; i++ {
 		(*completedLines)[i] = false
 	} // for
+
 } // initCompletedLines()
 
 // initialize tetris boolean map with falses(0)
@@ -41,25 +51,252 @@ func InitTetrisMap(tetrisMap *[Const.ROWS][Const.COLUMNS]rune) {
 	} // outer for
 } // initTetrisMap()
 
+// replace true in boolean tetris map with #s in the tetris map
+func UpdateTetrisMap(tetrisMap *[Const.ROWS][Const.COLUMNS]rune,
+	checkList *[Const.ROWS][Const.COLUMNS]bool) {
+
+	for row := 0; row < Const.ROWS; row++ {
+
+		for col := 0; col < Const.COLUMNS; col++ {
+
+			if (*checkList)[row][col] == true {
+				(*tetrisMap)[row][col] = '#'
+			} // if
+
+		} // for
+	} // for
+
+} // updateTetrisMap()
 
 // replace #s in tetris map with trues
-func CheckTetrisMap( tetrisMap *[Const.ROWS][Const.COLUMNS]rune, checkList *[Const.ROWS][Const.COLUMNS]bool, lengths *[Const.COLUMNS]int ) {
+func CheckTetrisMap(tetrisMap *[Const.ROWS][Const.COLUMNS]rune,
+	checkList *[Const.ROWS][Const.COLUMNS]bool,
+	lengths *[Const.COLUMNS]int) {
 
-    /* reversed checker to prevent stacked areas,
-     *   gone back to the original after having trouble with eliminateLines
-     */
-   for col := 0; col < Const.COLUMNS; col++ {
+	/* reversed checker to prevent stacked areas,
+	 *   gone back to the original after having trouble with eliminateLines
+	 */
+	for col := 0; col < Const.COLUMNS; col++ {
 
-        for row := 0; row < Const.ROWS; row++ {
+		for row := 0; row < Const.ROWS; row++ {
 
-            if (*tetrisMap)[row][col] == '#' {
+			if (*tetrisMap)[row][col] == '#' {
 
-                (*checkList)[row][col] = true;
-                (*lengths)[col]--;
-                //break;
+				(*checkList)[row][col] = true
+				(*lengths)[col]--
+				//break;
 
-            } // if
-        } // inner for
-    } // outer for
+			} // if
+		} // inner for
+	} // outer for
 
-} // void checkTetrisMap
+} // checkTetrisMap()
+
+// if line is copleted mark its place in the completedLines array
+func MarkDoneLines(tetrisBooleanMap *[Const.ROWS][Const.COLUMNS]bool,
+	completedLines *[Const.ROWS]bool,
+	eliminatedLines *int) {
+
+	for row := 0; row < Const.ROWS; row++ {
+
+		if (*tetrisBooleanMap)[row][0] == true &&
+			(*tetrisBooleanMap)[row][1] == true &&
+			(*tetrisBooleanMap)[row][2] == true &&
+			(*tetrisBooleanMap)[row][3] == true &&
+			(*tetrisBooleanMap)[row][4] == true &&
+			(*tetrisBooleanMap)[row][5] == true &&
+			(*tetrisBooleanMap)[row][6] == true &&
+			(*tetrisBooleanMap)[row][7] == true &&
+			(*tetrisBooleanMap)[row][8] == true &&
+			(*tetrisBooleanMap)[row][9] == true {
+
+			// I fucked up here not gonna lie
+			(*completedLines)[row] = true
+
+			// increase eliminated lines by one
+			(*eliminatedLines)++
+
+			/*for(int col = 0; col < COLUMNS; col++){
+
+			    if( tetrisBooleanMap[row][col] != 1 ) {
+			        break;
+			    }
+			    if( tetrisBooleanMap[row][9] == 1 ) {
+
+			    }
+
+			}*/
+
+		} // crazy if
+	} // outer for
+
+} // void markDoneLines
+
+// eliminate completed lines and shift upper lines down
+func EliminateLines(tetrisMap *[Const.ROWS][Const.COLUMNS]rune,
+	tetrisBooleanMap *[Const.ROWS][Const.COLUMNS]bool,
+	completedLines *[Const.ROWS]bool,
+	columnsLengths *[Const.COLUMNS]int) {
+
+	// look for completed(filled) lines from down to up, it's more logical that way :)
+	for row := Const.ROWS - 1; row >= 0; row-- {
+
+		if (*completedLines)[row] == true {
+
+			// set completion state to false
+			(*completedLines)[row] = false
+
+			// reset line
+			for col := 0; col < Const.COLUMNS; col++ {
+				(*tetrisMap)[row][col] = '.'
+				(*tetrisBooleanMap)[row][col] = false
+				// since there's an eleminated line so lengths are increased
+				(*columnsLengths)[col]++
+
+			} // inner for1
+
+			// shift rows down
+			// remeber we're going downnnn to up
+			for col := 0; col < Const.COLUMNS; col++ {
+				// rowEL is no spanish or mexican it's just (row at Eleminated Line)
+				for rowEL := row; rowEL > 0; rowEL-- {
+
+					(*tetrisMap)[rowEL][col] = (*tetrisMap)[rowEL-1][col]
+					(*tetrisBooleanMap)[rowEL][col] = (*tetrisBooleanMap)[rowEL-1][col]
+
+				} // inner inner for
+			} // inner for 2
+
+		} // if
+
+	} // outer for
+
+} // eliminateLines()
+
+// quit the game
+func PrintGameOverAndGTFOH() {
+	Common.Clear()
+	// print red game over
+	fmt.Println(Const.RED)
+	fmt.Println("GAME OVER !!!!")
+	fmt.Println(Const.RESET)
+	os.Exit(0)
+
+} // printGameOverAndGTFOH()
+
+/*transposedShape[2][3];
+  char eqNoneTransposedShape[2][3];
+*/
+
+func DropBlockOneRow(tetrisMap *[Const.ROWS][Const.COLUMNS]rune,
+	block *Common.Tetromino,
+	currX, currY,
+	destX int) {
+	//int rotation) {
+
+	// pi/2 rad rotation
+
+	// swapping height and width is really reallly important trust me
+	//swap(&block->height, &block->width);
+
+	/*
+	   for(; block->rotationsCounter <= block->rotation; block->rotationsCounter++) {
+
+
+	   for(int row = 0; row < block->width; row++) {
+
+	       for(int col = 0; col < block->height; col++) {
+
+	           eqNoneTransposedShape[row][col] = block->eqNone[col][row];
+
+	       }
+	   }
+
+	   for(int row = 0; row < block->width; row++) {
+
+	       for(int col = 0; col < block->height; col++) {
+
+	           transposedShape[row][col] = block->shape[col][row];
+
+	       }
+	   }
+
+	   swap(&block->height, &block->width);
+
+	   }
+	   //int tmp = block->width;
+	   //block->width = block->height;
+	   //block->height = tmp;
+
+	   int *x = &block->x;
+	   int *y = &block->y;
+
+	   // draw equivalent empty block on the tetris map
+	   for(int shapeRow = 0; shapeRow < block -> height; shapeRow++) {
+
+	       for(int shapeCol = 0; shapeCol < block -> width; shapeCol++) {
+
+	           if(block->rotation > 0) {
+	               tetrisMap[*y + shapeRow][*x + shapeCol] = eqNoneTransposedShape[shapeRow][shapeCol];
+	           }
+
+	           else {
+	               tetrisMap[*y + shapeRow][*x + shapeCol] = block -> eqNone[shapeRow][shapeCol];
+	           }
+	       } //
+
+	   } //
+
+	   // update coordinates
+	   *x = destX;
+	   (*y)++; // drop one block
+
+	   // draw block on the tetris map
+	   for(int shapeRow = 0; shapeRow < block -> height; shapeRow++) {
+
+	       for(int shapeCol = 0; shapeCol < block->width; shapeCol++) {
+
+	           if(block->rotation > 0) {
+	               tetrisMap[*y + shapeRow][*x + shapeCol] = transposedShape[shapeRow][shapeCol];
+	           }
+	           else {
+	               tetrisMap[*y + shapeRow][*x + shapeCol] = block -> shape[shapeRow][shapeCol];
+	           }
+	       } //
+
+	   } //
+	*/
+
+	// 0 rad rotation
+
+	x := &block.X
+	y := &block.Y
+
+	// draw equivalent empty block on the tetris map
+	for shapeRow := 0; shapeRow < (*block).Height; shapeRow++ {
+
+		for shapeCol := 0; shapeCol < (*block).Width; shapeCol++ {
+
+			(*tetrisMap)[*y+shapeRow][*x+shapeCol] = (*block).EqNone[shapeRow][shapeCol]
+
+		} //
+
+	} //
+
+	// update coordinates
+	*x = destX
+	(*y)++ // drop one block
+
+	// draw block on the tetris map
+	for shapeRow := 0; shapeRow < (*block).Height; shapeRow++ {
+
+		for shapeCol := 0; shapeCol < (*block).Width; shapeCol++ {
+
+			(*tetrisMap)[*y+shapeRow][*x+shapeCol] = (*block).Shape[shapeRow][shapeCol]
+
+		} //
+
+	} //
+	// 0 rad rotation
+
+} // dropBlockOneRow()
--- TheTetrisProject/main.go
+++ TheTetrisProject/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"time"
+	//"os"
 	Const "./Constants"
 	Common "./Shared"
 	TF "./TetrisFuncs"
@@ -21,13 +22,13 @@ func main() {
 
 	var cTetrisMainMap [Const.ROWS][Const.COLUMNS]rune
 	// mark each taken place with true
-	var bCheckList[Const.ROWS][Const.COLUMNS] bool
+	var bCheckList [Const.ROWS][Const.COLUMNS]bool
 	// columns lengths
-	var nColsLengths[Const.COLUMNS] int
-		// completed rows
-	var bCompletedLines[Const.ROWS] bool
-		// lines eliminated
-	//var nLines int = 0
+	var nColsLengths [Const.COLUMNS]int
+	// completed rows
+	var bCompletedLines [Const.ROWS]bool
+	// lines eliminated
+	var nLines int = 0
 
 	TF.InitLengths(&nColsLengths)
 	TF.InitCheckList(&bCheckList)
@@ -35,31 +36,27 @@ func main() {
 	TF.InitCompletedLines(&bCompletedLines)
 
 	// start dropping from the first row
-	var droppingRow int = -1;
+	var droppingRow int = -1
 	// dropping starts from the middle
-	var col = int(Const.COLUMNS / 2 - 1);
+	var col int = Const.COLUMNS/2 - 1
 
 	// print current tetris
-	Common.PrintMatrix(cTetrisMainMap);
+	Common.PrintMatrix(cTetrisMainMap)
 
+	var newCol = int(col)
 
-	var newCol = int(col);
-
-
-	/*Tetromino tetrominos[] = {square, straight,
+	/*var tetrominos [7]Tetromino = { square, straight,
 	                       lShape, lShapeInverse,
-	                       tShape, skew, skewInverse};
+	                       tShape, skew, skewInverse}
 
-	  int counter = 0;
+	 /* int counter = 0;
+*/
+	var block *Common.Tetromino
+	//block = &tetrominos[counter];
 
-	  Tetromino *block;
-	  //block = &tetrominos[counter];
-
-	  block = &skew;
-	  block->x = 4;
-	  block->y = 0;
-	*/
-
+	block = &skew
+	block.X = 4
+	block.Y = 0
 
 	var chr rune
 	//var key keyboard.Key
@@ -67,124 +64,244 @@ func main() {
 	// game loop
 	for {
 
-		if( droppingRow == nColsLengths[col]) {
-            TF.InitLengths(&nColsLengths)
-            TF.CheckTetrisMap(&cTetrisMainMap, &bCheckList, &nColsLengths);
-        }
+		block.Y = droppingRow - (block.Height - 1)
 
-		droppingRow++
+		// overlapping checker
+		if droppingRow == nColsLengths[col] {
+			fmt.Println(Const.RED)
+			fmt.Println("what up bitch")
+			fmt.Println(Const.RESET)
+			TF.InitLengths(&nColsLengths)
+			TF.CheckTetrisMap(&cTetrisMainMap, &bCheckList, &nColsLengths)
+		}
+
+		/*droppingRow++
 		// replace dropped character with a dot
-        if droppingRow < nColsLengths[newCol] && droppingRow > 0{
+		if droppingRow < nColsLengths[newCol] && droppingRow > 0 {
 
-            cTetrisMainMap[droppingRow - 1][newCol] = '.'
-        }
+			cTetrisMainMap[droppingRow-1][newCol] = '.'
+		}*/
 
-		if bCheckList[droppingRow][col] == true {
-            /*if(counter >= 6) {
-            counter = 0;
-        	} else {
-            	counter++;
-        	}*/
-
-            droppingRow = 0
-            continue
-        }
+		if 	droppingRow >= 0 &&
+			droppingRow <= nColsLengths[col] &&
+			bCheckList[droppingRow][col] == true {
+			/*if(counter >= 6) {
+			      counter = 0;
+			  	} else {
+			      	counter++;
+			  	}*/
+			fmt.Println(Const.RED)
+			fmt.Println("I Werk Bitch")
+			fmt.Println(Const.RESET)
+			//os.Exit(0)
+			droppingRow = 0
+			continue
+		}
 
 		go func() {
 			chr, _, _ = keyboard.GetSingleKey()
 
 		}()
 
-		time.Sleep(time.Millisecond * 500)
-
+		time.Sleep(time.Millisecond * 300)
 
 		// move left / right
-        if chr == 'A' || chr == 'a' {
-            newCol--
-        } else if chr == 'D' || chr == 'd' {
-            newCol++
-        }
-        /*else if chr == 'W' || chr == 'w' {
-            if block->rotation >= 3 {
-                block->rotation = 0
-                block->rotationsCounter = 0;
-            }
-            else {
-                block->rotation++;
-            }
-        }*/
+		if chr == 'A' || chr == 'a' {
+			newCol--
+		} else if chr == 'D' || chr == 'd' {
+			newCol++
+		}
+		/*else if chr == 'W' || chr == 'w' {
+		    if block->rotation >= 3 {
+		        block->rotation = 0
+		        block->rotationsCounter = 0;
+		    }
+		    else {
+		        block->rotation++;
+		    }
+		}*/
 
+		// setting boundaries
+		if newCol >= 9 {
+			newCol = 9
+		} else if newCol <= 0 {
+			newCol = 0
+		}
 
-        // setting boundaries
-        if newCol >= 9 {
-            newCol = 9
-        } else if newCol <= 0  {
-            newCol = 0
-        }
+		droppingRow++
+		if 	block.Y >= 0 &&
+			droppingRow >= 0 &&
+			droppingRow <= nColsLengths[col] {
+
+			TF.DropBlockOneRow(&cTetrisMainMap, block,
+						   col, droppingRow, newCol)
+		}
+
+		col = newCol
 
 		// overlapping checker
-        /*if( droppingRow == nColsLengths[col]) {
-            initLengths(colsLengths);
-            checkTetrisMap(tetrisMainMap, checkList, colsLengths);
-        }*/
+		/*if( droppingRow == nColsLengths[col]) {
+		    initLengths(colsLengths);
+		    checkTetrisMap(tetrisMainMap, checkList, colsLengths);
+		}*/
 
-        /*if( tetrisMainMap[0][col] == '#' && tetrisMainMap[1][col] == '#') {
-            printGameOverAndGTFOH();
-        }*/
+		/*if( tetrisMainMap[0][col] == '#' && tetrisMainMap[1][col] == '#') {
+		    printGameOverAndGTFOH();
+		}*/
 
+		/*if droppingRow < nColsLengths[newCol] {
+			cTetrisMainMap[droppingRow][newCol] = '#'
+		}*/
 
-
-
-
-		if( droppingRow < nColsLengths[newCol] ) {
-            cTetrisMainMap[droppingRow][newCol] = '#'
-        }
-		col = newCol
 		//fmt.Printf("rune: %v key: %v \n", chr, key)
 
-
 		Common.Clear()
+		TF.MarkDoneLines(&bCheckList, &bCompletedLines, &nLines)
+		TF.EliminateLines(&cTetrisMainMap, &bCheckList,
+							&bCompletedLines, &nColsLengths)
+		TF.UpdateTetrisMap(&cTetrisMainMap, &bCheckList)
 		Common.PrintMatrix(cTetrisMainMap)
+		fmt.Println(droppingRow, nColsLengths[col])
+
+		//// exists for debugging
+        fmt.Println("lines completed:")
+        for col := 0; col < Const.ROWS; col++ {
+
+            fmt.Printf("%v ", bCompletedLines[col])
+
+        }
+        fmt.Println("\nlines lengths:")
+        for col := 0; col < Const.COLUMNS; col++ {
+
+            fmt.Printf("%d ", nColsLengths[col])
+
+        }
+        fmt.Printf("\nLine: %d", nLines)
+        //// debugging budies
 
 	} // game loop
 
 }
 
 var (
+	straight Common.Tetromino = Common.Tetromino {
+		4,
+		1,
+		[4][2]rune{{'#', '\000'},
+			{'#', '\000'},
+			{'#', '\000'},
+			{'#', '\000'}},
 
-	straight Common.Tetromino = Common.Tetromino{
-			4,
-			1,
-			[4][2]rune{ {'#', '\000'},
-      					{'#', '\000'},
-      					{'#', '\000'},
-      					{'#', '\000'}},
+		[4][2]rune{{'.', '\000'},
+			{'.', '\000'},
+			{'.', '\000'},
+			{'.', '\000'}},
+		4,
+		0,
+		0,
+		0}
 
-			[4][2]rune{{'.', '\000'},
-			 			{'.', '\000'},
-						{'.', '\000'},
-						{'.', '\000'}},
-						4,
-						0,
-						0,
-						0}
+	skew Common.Tetromino = Common.Tetromino {
+		3,
+		2,
+		[4][2]rune{{'.', '#'},
+			{'#', '#'},
+			{'#', '.'},
+			{'\000', '\000'}},
+		[4][2]rune{{'.', '.'},
+			{'.', '.'},
+			{'.', '.'},
+			{'\000', '\000'}},
+		4,
+		0,
+		0,
+		0}
+/*
+	lShape Common.Tetromino = Common.Tetromino {
+    	3,
+	    2,
+    	[4][2]rune{ {'#', '.'},
+      				{'#', '.'},
+      				{'#', '#'},
+      				{'\0', '\0'}},
+
+    	[4][2]rune{ {'.', '.'},
+      				{'.', '.'},
+      				{'.', '.'},
+
+		4,
+    	0,
+    	0,
+    	0}
 
 
+	lShapeInverse Common.Tetromino = Common.Tetromino {
+	   	3,
+	    2,
+	   	[4][2]rune{ {'.', '#'},
+	   				{'.', '#'},
+	  				{'#', '#'},
+	  				{'\0', '\0'}},
 
-	skew Common.Tetromino = Common.Tetromino{
-			3,
-			2,
-			[4][2]rune{{'.', '#'},
-						{'#', '#'},
-						{'#', '.'},
-						{'\000', '\000'}},
-			[4][2]rune{{'.', '.'},
-			 			{'.', '.'},
-						{'.', '.'},
-						{'\000', '\000'}},
-						4,
-						0,
-						0,
-						0}
+	   	[4][2]rune{ {'.', '.'},
+	   				{'.', '.'},
+	   				{'.', '.'},
+
+		4,
+	    0,
+	    0,
+	    0}
+
+	tShape Common.Tetromino = Common.Tetromino {
+	   	3,
+	    2,
+	   	[4][2]rune{ {'#', '.'},
+	   				{'#', '#'},
+	  				{'#', '.'},
+	  				{'\0', '\0'}},
+
+	   	[4][2]rune{ {'.', '.'},
+	   				{'.', '.'},
+	   				{'.', '.'},
+
+		4,
+	    0,
+	    0,
+	    0}
+
+	skew Common.Tetromino = Common.Tetromino {
+	   	3,
+	    2,
+	   	[4][2]rune{ {'.', '#'},
+	   				{'#', '#'},
+	  				{'#', '.'},
+	  				{'\0', '\0'}},
+
+	   	[4][2]rune{ {'.', '.'},
+	   				{'.', '.'},
+	   				{'.', '.'},
+
+		4,
+	    0,
+	    0,
+	    0}
+
+	skew Common.Tetromino = Common.Tetromino {
+		3,
+		2,
+		[4][2]rune{ {'#', '.'},
+					{'#', '#'},
+					{'.', '#'},
+					{'\0', '\0'}},
+
+		[4][2]rune{ {'.', '.'},
+					{'.', '.'},
+					{'.', '.'},
+
+		4,
+		0,
+		0,
+		0}
+*/
 
 )
